bb77c1639d76e656b2d9efccaba62ef9404b9bd8
AlgoVoyager <nishantdewangan2002@gmail.com>
2026-01-15 00:02:29 +0530

    final

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,8 @@
-import dotenv from "dotenv"
 import express from "express";
 import { connectDB } from "./utils/db.js";
 import { v1Router } from "./routes/index.js";
+import { PORT } from "./utils/config.js"
 
-dotenv.config()
 connectDB()
 
 const app = express();
@@ -11,7 +10,6 @@ app.use(express.json())
 app.use('/api/v1', v1Router);
 // app.use('/',()=>{console.log(1)})
 
-const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
     console.log("Server running on port: ", PORT)
 })
